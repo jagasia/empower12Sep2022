5ffd570c9ed20b2718f47a1e0c1d0b4d8cbc54b9
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-09-15 19:05:22 +0530

    solution2

---
+++ java/15-sep-2022-arrays/src/MaximumProduct.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaximumProduct {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		int arr[]=new int[size];
+		for(int i=0;i<size;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		int max=arr[size-1];
+		boolean flag=false;
+		for(int i=0;i<size;i++)
+		{
+			int result=0;
+			if(arr[i]!=max)
+			{
+				result=max/arr[i];
+				//check if this result is found in array
+				for(int j=0;j<size;j++)
+				{
+					if(result==arr[j])
+					{
+						//found
+						flag=true;
+						break;
+					}
+				}
+			}
+		}
+		if(flag)
+		{
+			System.out.println(max);
+		}else
+		{
+			System.out.println(-1);
+		}
+	}
+
+}
