e98758dd05825c052e9265160bf0a5f9e3e2759b
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-09-26 12:12:23 +0530

    26Sep12noon

---
+++ java/26-sep-stream-1/src/App.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class App {
+
+	public static void main(String[] args) {
+		List<String> countryList=new ArrayList<String>();
+		countryList.add("India");
+		countryList.add("Australia");
+		countryList.add("Pakistan");
+		countryList.add("China");
+		countryList.add("Korea");
+		countryList.add("South Korea");
+		countryList.add("Sri lanka");
+		countryList.add("Japan");
+		countryList.add("Italy");
+		countryList.add("Bangladesh");
+		countryList.add("Nepal");
+		countryList.add("Bhutan");
+		countryList.add("Afghanistan");
+		countryList.add("Tanzania");
+		countryList.add("Germany");
+		
+		countryList.stream()
+		.filter((x)->x.length()>10)
+		.map((x)->x.toUpperCase())
+		.sorted()
+		.forEach((x)->System.out.println(x));
+		
+	}
+
+}
---
+++ java/26-sep-stream-1/src/Dummy.java
@@ -0,0 +1,14 @@
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class Dummy implements Consumer<String>
+{
+
+	@Override
+	public void accept(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
