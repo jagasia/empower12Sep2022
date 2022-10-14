fd2743a92def1df475f5f16889f454a50c0bfc7c
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-10-14 13:13:05 +0530

    14Oct1pm

---
+++ JS/myscript.js
@@ -0,0 +1,8 @@
+function fn1()
+{
+    // alert('Hello world');
+    var x=parseInt(document.getElementById("no1").value);
+    var y=parseInt(document.getElementById("no2").value);
+    alert(x+y)
+    
+}
