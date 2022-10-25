ee333a2336e0a1534ce32bf98d1a29c122d6a3ee
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2022-10-25 11:07:26 +0530

    24Oct11am

---
+++ angular/oct25-demo1/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm:any;
+
+  constructor(private fb:FormBuilder) {
+    this.loginForm=this.fb.group({
+      username:[],
+      password:[]
+    });
+   }
+
+  ngOnInit(): void {
+  }
+
+}
